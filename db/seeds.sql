8f609ffb19cc6221994fb808044831b2bcdf4b73
Matt Benson <rabenson6@msn.com>
2020-03-14 17:20:53 -0700

    working post

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES('bleu cheese', false);
+INSERT INTO burger (burger_name, devoured) VALUES('rodeo', false);
+INSERT INTO burger (burger_name, devoured) VALUES('impossible', false);
